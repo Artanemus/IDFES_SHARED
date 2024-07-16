05850226c2945a1ca51020c2cb4d35799742f175
Artanemus <69775305+Artanemus@users.noreply.github.com>
2024-07-16 11:36:31 +1000

    PS script icon running

--- ps/build_icons.ps1
+++ ps/build_icons.ps1
@@ -1,13 +1,12 @@
-# Set-Location ..\Documents\GitHub\SCM_ASSETS\ICONS\
+#Set-Location ..$Ev = $Env:HOMEDRIVE + $Env:HOMEPATH  + '\Documents\GitHub\IDFES_SHARED\assets\'
 
-$Ev = $Env:HOMEDRIVE + $Env:HOMEPATH  + '\Documents\GitHub\SCM_ASSETS\ICONS\'
+$infile = $Env:HOMEDRIVE + $Env:HOMEPATH  + '\Documents\GitHub\IDFES_SHARED\assets\IDFES-logo-small.png'
+# magick needs a fully qualified pathname - no abbrev allowed!
+$outfileICO = $Env:HOMEDRIVE + $Env:HOMEPATH  + '\Documents\GitHub\IDFES_SHARED\assets\IDFES.ico'
 
-$infile = $Ev + 'TimeKeeper.png'
-
-$outpath = $Ev
-$outfileICO = $outpath + 'TimeKeeper.ico'
-$outfileWUP150 = $outpath + 'TimeKeeper_150x150.png'
-$outfileWUP44 = $outpath + 'TimeKeeper_44x44.png'
+$outfileWUP128 = $Env:HOMEDRIVE + $Env:HOMEPATH  + '\Documents\GitHub\IDFES_SHARED\assets\IDFES_128x128.png'
+$outfileWUP64 = $Env:HOMEDRIVE + $Env:HOMEPATH  + '\Documents\GitHub\IDFES_SHARED\assets\IDFES_64x64.png'
+$outfileWUP48 = $Env:HOMEDRIVE + $Env:HOMEPATH  + '\Documents\GitHub\IDFES_SHARED\assets\IDFES_48x48.png'
 
 if (Test-Path -Path $outfileICO -PathType Leaf) {
     Remove-Item $outfileICO 
@@ -15,15 +14,21 @@ if (Test-Path -Path $outfileICO -PathType Leaf) {
 
 magick convert $infile -flatten -resize 256x256 -alpha off -background white -define icon:auto-resize="256,128,96,64,48,32,16" $outfileICO
 
-
-if (Test-Path -Path $outfileWUP150 -PathType Leaf) {
-    Remove-Item $outfileWUP150
+if (Test-Path -Path $outfileWUP128 -PathType Leaf) {
+    Remove-Item $outfileWUP128
 }
 
-magick convert $infile -flatten -resize 150x150 $outfileWUP150
+magick convert $infile -flatten -resize 128x128 $outfileWUP128
 
-if (Test-Path -Path $outfileWUP44 -PathType Leaf) {
-    Remove-Item $outfileWUP44
+
+if (Test-Path -Path $outfileWUP64 -PathType Leaf) {
+    Remove-Item $outfileWUP64
 }
 
-magick convert $infile -flatten -resize 44x44 $outfileWUP44
+magick convert $infile -flatten -resize 64x64 $outfileWUP64
+
+if (Test-Path -Path $outfileWUP48 -PathType Leaf) {
+    Remove-Item $outfileWUP48
+}
+
+magick convert $infile -flatten -resize 48x48 $outfileWUP48
